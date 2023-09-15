6ea0e69c45bff4e9be62b7a850db0990f78ee747
Leo5233 <leohsieh53@gmail.com>
2023-09-15 17:01:03 +0800

    Add annotation

--- main.js
+++ main.js
@@ -41,7 +41,6 @@ function generateTableRow(allData){
 function getPosition(element) {
   let x = 0;
   let y = 0;
-  // 搭配上面的示意圖可比較輕鬆理解為何要這麼計算
   while (element) {
     x += element.offsetLeft - element.scrollLeft + element.clientLeft;
     y += element.offsetTop - element.scrollLeft + element.clientTop;
